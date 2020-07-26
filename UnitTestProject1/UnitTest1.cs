a2cef444251bab97ef20c76021b0714ac358c306
V-Kunal Verma <Kunal.Verma@blackbaud.me>
2020-07-26 13:42:43 +0530

    forw

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program program = new Program();
+            var  r=program.GetString();
+
+            Assert.IsNotNull(r);
+        }
+    }
+}
