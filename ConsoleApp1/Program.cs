168d8bc2aa58350f21971737cef69c21452a2c4f
V-Kunal Verma <Kunal.Verma@blackbaud.me>
2020-07-26 13:40:02 +0530

    console app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            string w = p.GetString();
+            Console.WriteLine(w);
+        }
+
+        public  string GetString()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
